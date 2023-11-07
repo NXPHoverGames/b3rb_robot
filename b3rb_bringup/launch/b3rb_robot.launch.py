443e4e5d193797463f7df022e3eaf6d9409839ba
James Goppert <james.goppert@gmail.com>
2023-11-07 01:35:23 -0500

    Update script.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- b3rb_bringup/launch/b3rb_robot.launch.py
+++ b3rb_bringup/launch/b3rb_robot.launch.py
@@ -1,15 +1,12 @@
-import os
-from pathlib import Path
-
+from os import environ
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch.conditions import IfCondition, LaunchConfigurationEquals
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
+from launch.actions import ExecuteProcess
+from launch.conditions import LaunchConfigurationEquals, IfCondition
 from launch_ros.actions import Node
 
 
@@ -29,157 +26,98 @@ ARGUMENTS = [
     DeclareLaunchArgument('laser', default_value='true',
                           choices=['true', 'false'],
                           description='Run laser'),
-    DeclareLaunchArgument('synapse', default_value='true',
+    DeclareLaunchArgument('synapse_ros', default_value='true',
                           choices=['true', 'false'],
                           description='Run synapse_ros'),
-    DeclareLaunchArgument('use_sim_time', default_value='false',
+    DeclareLaunchArgument('description', default_value='true',
                           choices=['true', 'false'],
-                          description='use_sim_time'),
-    DeclareLaunchArgument('model', default_value='lidar',
-                          choices=['base', 'lidar'],
-                          description='B3RB Model'),
-    DeclareLaunchArgument('robot_name', default_value='b3rb',
-                          description='Robot name')
+                          description='Run description'),
+    DeclareLaunchArgument('use_sim_time', default_value='true',
+                          choices=['true', 'false'],
+                          description='Use sim time'),
+    DeclareLaunchArgument('log_level', default_value='error',
+                          choices=['info', 'warn', 'error'],
+                          description='log level'),
 ]
 
 
 def generate_launch_description():
-
-    # Directories
-    pkg_b3rb_bringup = get_package_share_directory(
-        'b3rb_bringup')
-    pkg_b3rb_description = get_package_share_directory(
-        'b3rb_description')
-    pkg_b3rb_nav2 = get_package_share_directory(
-        'b3rb_nav2')
-    pkg_synapse_ros = get_package_share_directory(
-        'synapse_ros')
-    pkg_corti = get_package_share_directory('corti')
-
-    # Paths
-    nav2_launch = PathJoinSubstitution(
-        [pkg_b3rb_nav2, 'launch', 'nav2.launch.py'])
-    corti_launch = PathJoinSubstitution(
-        [pkg_corti, 'launch', 'corti.launch.py'])
-    slam_launch = PathJoinSubstitution(
-        [pkg_b3rb_nav2, 'launch', 'slam.launch.py'])
-    localization_launch = PathJoinSubstitution(
-        [pkg_b3rb_nav2, 'launch', 'localization.launch.py'])
-    robot_description_launch = PathJoinSubstitution(
-        [pkg_b3rb_description, 'launch', 'robot_description.launch.py'])
-    laser_launch = PathJoinSubstitution(
-        [pkg_b3rb_bringup, 'launch', 'laser.launch.py'])
-    synapse_ros_launch = PathJoinSubstitution(
-        [pkg_synapse_ros, 'launch', 'synapse_ros.launch.py'])
-
-    # Parameters
-    declare_map_yaml_cmd = DeclareLaunchArgument(
-        'map',
-        default_value=PathJoinSubstitution(
-            [pkg_b3rb_nav2, 'maps', 'depot.yaml']),
-        description='Full path to map yaml file to load')
-
-    # laser
-    laser = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([laser_launch]),
-        condition=IfCondition(LaunchConfiguration('laser')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time')),
-            ('stl27l', 'true')
-        ]
+    synapse_ros = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('synapse_ros'), 'launch', 'synapse_ros.launch.py'])]),
+        condition=IfCondition(LaunchConfiguration('synapse_ros')),
+        launch_arguments=[('host', ['192.0.2.1']),
+                          ('port', '4242'),
+                          ('use_sim_time', LaunchConfiguration('use_sim_time'))]
     )
 
-    # Synapse ROS
-    synapse_ros = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([synapse_ros_launch]),
-        condition=IfCondition(LaunchConfiguration('synapse')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time')),
-            ('host', '192.0.2.1'),
-            ('port', '4242')
-        ]
+    laser = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('b3rb_bringup'), 'launch', 'laser.launch.py'])]),
+        condition=IfCondition(LaunchConfiguration('laser')),
+        launch_arguments=[('stl27l', 'true')
+                          ('use_sim_time', LaunchConfiguration('use_sim_time'))]
     )
 
     # Robot description
     robot_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([robot_description_launch]),
-        launch_arguments=[('model', LaunchConfiguration('model')),
-                          ('use_sim_time', LaunchConfiguration('use_sim_time'))]
-    )
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory('b3rb_description'), 'launch', 'robot_description.launch.py'])]),
+        condition=IfCondition(LaunchConfiguration('description')),
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
-    # NAV2
     nav2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([nav2_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'b3rb_nav2'), 'launch', 'nav2.launch.py'])]),
         condition=IfCondition(LaunchConfiguration('nav2')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
-    # Corti
     corti = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([corti_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory('corti'), 'launch', 'corti.launch.py'])]),
         condition=IfCondition(LaunchConfiguration('corti')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
     slam = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([slam_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'b3rb_nav2'), 'launch', 'slam.launch.py'])]),
         condition=LaunchConfigurationEquals('localization', 'slam'),
-        launch_arguments=[
-            ('sync', LaunchConfiguration('sync')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time')),
+            ('sync', LaunchConfiguration('sync'))])
 
     localization = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([localization_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'b3rb_nav2'), 'launch', 'localization.launch.py'])]),
         condition=LaunchConfigurationEquals('localization', 'localization'),
-        launch_arguments=[
-            ('map', LaunchConfiguration('map')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
-    
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time')),
+            ('map', PathJoinSubstitution([get_package_share_directory(
+                'b3rb_nav2'), 'maps', LaunchConfiguration('map_yaml')]))])
+
     tf_to_odom = Node(
+        condition=IfCondition(LaunchConfiguration('corti')),
         package='corti',
         executable='tf_to_odom',
         output='screen',
-        condition=IfCondition(LaunchConfiguration('corti')),
         parameters=[{
             'base_frame': 'map',
             'target_frame': 'base_link',
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
             }],
         remappings=[
             ('/odom', '/cerebri/in/odometry')
             ])
 
-    odom_to_tf = Node(
-        package='corti',
-        executable='odom_to_tf',
-        output='screen',
-        parameters=[{
-            'async': False,
-            'sync_dt': .02,
-            }],
-        remappings=[
-            ('/odom', '/odom')
-            ])
-
-
     # Define LaunchDescription variable
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(declare_map_yaml_cmd)
-    ld.add_action(laser)
-    ld.add_action(robot_description)
-    ld.add_action(synapse_ros)
-    ld.add_action(nav2)
-    ld.add_action(corti)
-    ld.add_action(slam)
-    ld.add_action(localization)
-    ld.add_action(odom_to_tf)
-    ld.add_action(tf_to_odom)
-    return ld
-
+    return LaunchDescription(ARGUMENTS + [
+        robot_description,
+        synapse_ros,
+        nav2,
+        corti,
+        slam,
+	laser,
+        localization,
+        tf_to_odom
+    ])
